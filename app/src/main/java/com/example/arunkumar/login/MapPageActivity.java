b3a157c2446863d3a1b48fe3203d50af7c2be7c3
Arunkumar Nandagopal <arunsite@gmail.com>
2018-04-20 10:15:44 +0530

    Added Map Page - Able to view map

--- app/src/main/java/com/example/arunkumar/login/MapPageActivity.java
+++ app/src/main/java/com/example/arunkumar/login/MapPageActivity.java
@@ -1,11 +1,15 @@
 package com.example.arunkumar.login;
 
+import android.location.Location;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.esri.android.map.GraphicsLayer;
 import com.esri.android.map.MapView;
 import com.esri.android.map.ags.ArcGISFeatureLayer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
 public class MapPageActivity extends AppCompatActivity {
 
@@ -13,6 +17,10 @@ public class MapPageActivity extends AppCompatActivity {
     ArcGISFeatureLayer mFeatureLayer;
     GraphicsLayer mGraphicsLayer;
     String mFeatureServiceURL;
+
+    private GPSTracker gpsTracker;
+    private Location mLocation;
+    double latitude, longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +37,13 @@ public class MapPageActivity extends AppCompatActivity {
 // Add Graphics layer to the MapView
         mGraphicsLayer = new GraphicsLayer();
         mMapView.addLayer(mGraphicsLayer);
+
+        gpsTracker = new GPSTracker(getApplicationContext());
+        mLocation = gpsTracker.getLocation();
+
+        latitude = mLocation.getLatitude();
+        longitude = mLocation.getLongitude();
+
+
     }
 }
